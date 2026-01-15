934c3dba0421d18383176044ea92ab6025589f56
forger6969 <saidazim186@gmail.com>
2026-01-15 23:59:47 +0500

    add model group and exam

---
+++ groups/group.controller.js
@@ -0,0 +1,11 @@
+const Group = require("./group.model");
+
+const createGroup = async (req, res) => {
+  try {
+
+    
+
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
---
+++ groups/group.model.js
@@ -0,0 +1,18 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const groupSchema = mongoose.Schema({
+  groupName: { type: String, required: true },
+  groupDescribe: { type: String, default: "" },
+  students: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  avatar: {
+    type: String,
+    default:
+      "https://media.licdn.com/dms/image/sync/v2/D4D27AQF-rPowGZp8QA/articleshare-shrink_800/articleshare-shrink_800/0/1729615523861?e=2147483647&v=beta&t=vd4PiWwC7S8HwDvyd8q57Y2LppPmDqi0cna6KImOT0o",
+  },
+  groupPerformance: { type: Number, default: 0 },
+  groupTime: { type: String, default: null },
+  groupDay: { type: String, enum: ["even", "odd"] }  
+});
+
+const Group = mongoose.model("Group" , groupSchema)
+module.exports = Group
